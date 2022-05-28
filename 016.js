ef0b35255b2f747ae0bdcd590fea06bea7102517
Carlos <caalb.craft@gmail.com>
2022-05-28 02:23:04 -0300

    exercicio

---
+++ 016.js
@@ -0,0 +1,31 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+function bmi(weight, height) {
+    const bmi = weight / (height * height) 
+    console.log(bmi)
+    if(bmi <= 18.5) {
+        return "Underweight"
+    } else if(bmi <= 25) {
+        return "Normal"
+    } else if(bmi <= 30) {
+        return "Overweight"
+    } else return "Obese"
+}
+
+function bmi2(weight, height) {
+    const bmi = weight / (height * height) 
+    return bmi <= 18.5 ? "Underweight" : bmi <= 25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese"
+}
+
+
+console.log(bmi2(80, 1.80))
+
